b4fecb041c7106a638fc17e24fe347e10a07b530
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-12-05 12:39:26 -0500

    created user new form

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,28 +1,29 @@
 class UsersController < ApplicationController
 
   def new
-
+    @user = User.new
   end
 
   def create
-
+    @user = User.create(user_params(:first_name, :last_name, :birthdate))
   end
 
   def show
-
+    @user = User.find(params[:id])
   end
 
 
   def edit
-
+    @user = User.find(params[:id])
   end
 
   def update
-
+    @user = User.find(params[:id])
   end
 
   def destroy
-
+    @user = User.find(params[:id])
+    @user.destroy 
   end
 
   private
