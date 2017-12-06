e6003d7093ca580415ac002c468beb3ee0083fbe
Yuliya Yasenetska <yuliya.yasenetska@flatironschool.com>
2017-12-05 21:00:12 -0500

    created events categories and embeded map to each event

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,12 @@
 class CategoriesController < ApplicationController
 
   def index
-    @category = Category.all
+    @categories = Category.all
   end
 
   def show
-    
+    @category = Category.find(params[:id])
   end
-
 
 
   private
